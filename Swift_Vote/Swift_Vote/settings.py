dfe09ca67b89447732a472669546c52b17774728
Psycho-not0078 <snbhat0708@gmail.com>
2021-11-17 19:38:43 +0530

    changed db to sql

--- Swift_Vote/Swift_Vote/settings.py
+++ Swift_Vote/Swift_Vote/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Swift_Vote.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my-app-db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
--- Swift_Vote/frontend/models.py
+++ Swift_Vote/frontend/models.py
@@ -1,3 +1,40 @@
+from typing_extensions import Required
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from datetime import date
 
-# Create your models here.
+uType=(("official","Official"),("Voter","Voter"),("Candidate","Candidate"))
+aType=(("official","Official"),("Normie","Normie"))
+class userDetails(AbstractUser):
+    uid=models.AutoField(primary_key=True)
+    email=models.EmailField(_("email_address"))
+    fName=models.CharField(max_length=30)
+    lName=models.CharField(max_length=30)
+    contactNumber=models.CharField(max_length=10)
+    username=models.CharField(max_length=30)
+    DOB=models.DateField()
+    documentLocation=models.CharField(max_length=500)
+    # password=models.TextField()
+    Address=models.TextField()
+    type=models.CharField(
+        max_length = 20,
+        choices = uType,
+        )
+    USERNAME_FIELD='username'
+    REQUIRED_FIELDS=['Fname','Lname','contactNumber','username','email','DOB',"documentLocation"]
+    EMAIL_FIELD='email'
+
+
+class Accounts(models.Model):
+    aid=models.IntegerField(primary_key=True)
+    accountType=models.CharField(
+        max_length = 20,
+        choices = aType,
+        )
+    accountString=models.CharField(max_length=250)
+    accountPrivate=models.TextField()
+    accountPublic=models.TextField()
+    assigned_to = models.ForeignKey(userDetails,to_field="uid",null=True,blank=True)
+
+    
