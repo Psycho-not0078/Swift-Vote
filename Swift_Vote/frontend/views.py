3191f7d9b7a08dea8351ecb89a147ee21f062f45
Jatin Singal <jatinsingal2@gmail.com>
2021-11-17 11:45:45 +0530

    Login Sign Up

--- Swift_Vote/frontend/views.py
+++ Swift_Vote/frontend/views.py
@@ -1,17 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+# from .models import userDetails
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.models import auth
 # Create your views here.
 def index(request):
     return render(request,"home.html")
 
 def login(request):
-    return render(request,"Log In.html")
+    # if request.method == 'POST':
+    #     email = str(request.POST['email']).lower()
+    #     password = request.POST['password']
+    #     try:
+    #         user = userDetails.objects.get(email=email)
+    #         if check_password(password, user.password):
+    #             user = auth.authenticate(request, email=email, password=password)
+    #             auth.login(request, user)
+    #             return redirect('/')
+    #         else:
+    #             return render(request, 'Log In.html', {'msg': 'Invalid Credentials'})
+    #     except Exception as e:
+    #         return render(request, 'Log In.html', {'msg': 'Invalid Credentials' + str(e)})
+    # else:
+        return render(request,"Log In.html")
+
+def register(request):
+    # if request.method == 'POST':
+    #     email = request.POST['Email']
+    #     pwd = request.POST['Password']
+    #     userType = request.POST['flexRadioDefault']
+    #     user = userDetails.objects.create_user(
+    #         email=email, password=pwd, type=userType)
+    #     user.save()
+    #     return redirect('/login')
+    # else:
+        return render(request,"Sign Up.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
 def error(request):
     return render(request,"errorpage.html")
-
-def register(request):
-    return render(request,"Sign Up.html")
 
 def candidateDB(request):
     return render(request,"candidate_db.html")
