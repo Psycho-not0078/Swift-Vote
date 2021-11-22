5594662106e7aae24fd6b71f696042acb88d87ad
sarthak248 <65854909+sarthak-banerjee@users.noreply.github.com>
2021-11-22 17:42:31 +0530

    Revert "minor changes for review"
    This reverts commit cf6ec04591bc7b13b3696f21a4295a7a624612f5.

--- Swift_Vote/Swift_Vote/settings.py
+++ Swift_Vote/Swift_Vote/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,10 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'my-app-db',
-        'USER': 'user#123',
-        'PASSWORD': 'password#123',
-        'HOST': '127.0.0.1',
-        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+         'PORT': 3306,
      }
 }
 # DATABASES = {
