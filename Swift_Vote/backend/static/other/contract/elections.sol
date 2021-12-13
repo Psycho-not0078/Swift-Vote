ed82109cdee8ff166c8c801fbaae55ea6bd32b4c
Psycho-not0078 <snbhat0708@gmail.com>
2021-12-13 16:55:02 +0530

    changes regarding smartcontract

--- Swift_Vote/backend/static/other/contract/elections.sol
+++ Swift_Vote/backend/static/other/contract/elections.sol
@@ -1,36 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 contract Election {
-    struct vote { //maping a vote to candidate
+    struct vt { //maping a vote to candidate
         uint256 id;
         uint256 cid;//candidate id
-        string location;
+        string context;
         string date;
     }
     
     struct candidate {
         uint256 id;
         string name;
-        string location;
+        string context;
     }
     
     struct user {
         uint256 id;
+        string tag;
+        string context;
         uint256 ablity_to_vote;
-        uint256 ablity_to_add; 
+        uint256 ablity_to_add;
+        uint256 ablity_to_change; 
     }
+    
     uint256 voteCount;
     uint256 candidateCount;
     uint256 userCount;
-    // string[] returner;
-    
-    mapping(address=>uint256) userAddress;
-    mapping(uint256=>user) users;
-    mapping(address=>uint256) voteAddress;
-    mapping(uint256=>vote) votes;
-    mapping(address=>uint256) candidateAddress;
+    uint256 voterCount;
+
+    mapping(uint256=>address) userAddress;
+    mapping(address=>user) users;
     mapping(uint256=>candidate) candidates; 
-    
+    mapping(uint256=>vt) votes;
+
     constructor(){
         init(msg.sender);
     }
@@ -38,79 +43,109 @@ contract Election {
     function init(address _ad) private{
         //adding the init user 
         userCount+=1;
-        userAddress[_ad]=userCount;
-        users[userAddress[_ad]]=user(userCount,0,1);
+        userAddress[userCount]=_ad;
+        users[_ad]=user(userCount,"admin","central",0,1,1);
     }
     
-    function addUser(address _ad) public{
+    function addUser(address _ad, string memory _context) public{
         //function to add users, be it candidates or voters or officials.
-        require(users[userAddress[msg.sender]].ablity_to_add==1,"the role doesnt allow it");
+        require(users[msg.sender].ablity_to_add==1,"the role doesnt allow it");
         userCount+=1;
-        userAddress[_ad]=userCount;
-        users[userAddress[_ad]]=user(userCount,0,0);
-    }
-    
-    function userDetails(address _ad) public view returns (user memory){
-        //a tester function and for manual verification of specific user update.
-        require(users[userAddress[msg.sender]].ablity_to_add==1,"the role doesnt allow it");
-        return users[userAddress[_ad]];
+        voterCount+=1;
+        userAddress[userCount]=_ad;
+        users[_ad]=user(userCount,"user",_context,0,0,0);
+    }  
+
+
+    function addCandidate( string memory _name, string memory _context) public {
+        //function to add a candidate to the system
+        require(users[msg.sender].ablity_to_add==1,"the role doesnt allow it");
+        candidateCount+=1;
+        candidates[candidateCount]=candidate(candidateCount, _name, _context);
     }
 
-    function listCandidates() public view returns (string[] memory,string[] memory){
-        // a viewer function to get the list of candidates to conduct voting
-        string[] memory _names = new string[](uint256(candidateCount)); 
-        string[] memory _locations = new string[](uint256(candidateCount)); 
-        for (uint256 _i=0; _i<uint256(candidateCount); _i+=1){
-            _names[_i]=candidates[_i+1].name;
-            _locations[_i]=candidates[_i+1].location;
-        }
-        return (_names,_locations);
-    }
-    
-    function listlocCandidate(string memory _location) public view returns (string[] memory) {
-        //a viwer function to get list of candidates that are in a specific location
-        string[] memory _names = new string[](uint256(candidateCount)); 
-        for (uint256 _i=0; _i<uint256(candidateCount); _i+=1){
-            if (keccak256(bytes(candidates[_i+1].location))==keccak256(bytes(_location))){//comparing the hash of strings to check if they are the same 
-                _names[_i]=candidates[_i+1].name;                                         //this bcause solidity doesn have its own string related functions
-            }
-        }
-        return _names;
-    }
-    
-    function addCandidate( address _ad,string memory _name, string memory _location) public {
-        //function to add a candidate to the system
-        require(users[userAddress[msg.sender]].ablity_to_add==1,"the role doesnt allow it");
-        candidateCount+=1;
-        candidateAddress[_ad]=candidateCount;
-        candidates[candidateAddress[_ad]]=candidate(candidateCount, _name, _location);
-    }
-    event changedUser(user u);
-    function changeAblity(uint256 _id, uint256 _ablityType, uint256 _value) public {
-        //function to change the add ablity or the vote ablity
-        require(users[userAddress[msg.sender]].ablity_to_add==1,"the role doesnt allow it");
+    function changeAblity(address _ad, uint256 _ablityType, uint256 _value) public {
+        require(users[msg.sender].ablity_to_change==1,"the role doesnt allow it");
         if (_ablityType==1){
-            users[_id].ablity_to_vote=_value;
+            users[_ad].ablity_to_vote=_value;
         }
-        else{
-            users[_id].ablity_to_add=_value;
+        else if(_ablityType==2){
+            users[_ad].ablity_to_add=_value;
         }
-        //have to add an event here
+        else if(_ablityType==3){
+            users[_ad].ablity_to_change=_value;
+        }
     }
-    function DisableAblity(address _ad) private { 
+
+    function disableVoteAblity(address _ad) private { 
         //function to not allow a person to vote more than once.
-        if (users[userAddress[_ad]].ablity_to_vote==1) {//will be called when vote function is called
-            users[userAddress[_ad]].ablity_to_vote=0;
+        if (users[_ad].ablity_to_vote==1) {//will be called when vote function is called
+            users[_ad].ablity_to_vote=0;
         }
         //have to write an event here
     }
-    function cvote(uint256 _cid,string memory _location,string memory _date) public {
-        //cvote for cast vote
-        require(users[userAddress[msg.sender]].ablity_to_vote==1,"error 403");
-        require(_cid<=candidateCount,"error 404");
-        require(keccak256(bytes(candidates[_cid].location))==keccak256(bytes(_location)),"error 404");
-        DisableAblity(msg.sender);//this is to not allow a person to vote more than once
+
+    function listVoters() public view returns (address  [] memory){
+        // require(keccak256(bytes(users[msg.sender].tag))==keccak256(bytes("admin")),"the role doesnt allow it");
+        address[] memory addressesses = new address[](voterCount);
+        uint256 i=1;
+        uint256 pos=0;
+        for(i=1;i<=userCount;i++){
+            if(users[userAddress[i]].ablity_to_vote==1){
+                addressesses[pos]=userAddress[i];
+                pos+=1;
+            }
+        }
+        return addressesses;
+    }
+
+    function listOfficials() public view returns (address  [] memory){
+        require(keccak256(bytes(users[msg.sender].tag))==keccak256(bytes("admin")),"the role doesnt allow it");
+        address[] memory addressesses = new address[](voterCount);
+        uint256 i=1;
+        uint256 pos=0;
+        for(i=1;i<=userCount;i++){
+            if((users[userAddress[i]].ablity_to_add==1 || users[userAddress[i]].ablity_to_change==1) && users[userAddress[i]].ablity_to_vote==0){
+                addressesses[pos]=userAddress[i];
+                pos+=1;
+            }
+        }
+        return addressesses;
+    }
+
+    function listCandidates(string memory _context)public view returns (string[] memory name, uint256[] memory id){
+        require(keccak256(bytes(users[msg.sender].tag))==keccak256(bytes("user")),"the role doesnt allow it");
+        string[] memory names=new string[](candidateCount);
+        uint256[] memory ids=new uint256[](candidateCount);
+        uint256 i=1;
+        uint256 pos=0;
+        for(i=1;i<=candidateCount;i++){
+            if(keccak256(bytes(candidates[i].context))==keccak256(bytes(_context))){
+                ids[pos]=candidates[i].id;
+                names[pos]=candidates[i].name;
+                pos+=1;
+            }
+        }
+        return (names,ids);
+    }
+
+    function vote(uint256 _id, string memory _context,string memory _date) public{
+        require(users[msg.sender].ablity_to_vote==1,"the role doesnt allow it");
+        require(keccak256(bytes(users[msg.sender].tag))==keccak256(bytes("user")),"the role doesnt allow it");
+        require(keccak256(bytes(users[msg.sender].context))==keccak256(bytes(candidates[_id].context)),"error 404");
         voteCount+=1;
-        votes[voteCount]=vote(voteCount,_cid,_location,_date);
+        votes[voteCount]=vt(voteCount, _id, _context, _date);
+        disableVoteAblity(msg.sender);
+    }
+    
+    function countVote(uint256 _id) public view returns (uint256 _count){
+        uint256 _vtcount=0;
+        uint256 i=1;
+        for(i=1;i<=voteCount;i++){
+            if (votes[i].cid==_id){
+                _vtcount+=1;
+            }
+        }
+        return _vtcount;
     }
 }
