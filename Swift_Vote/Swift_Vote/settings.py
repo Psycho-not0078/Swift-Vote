ec7193c1776f31d64f861b8c35851a17b8567ece
Psycho-not0078 <snbhat0708@gmail.com>
2021-12-15 21:35:57 +0530

    changed db to mariadb

--- Swift_Vote/Swift_Vote/settings.py
+++ Swift_Vote/Swift_Vote/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'Swift_Vote.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'my-app-db',
-#         'USER': 'toor',
-#         'PASSWORD': 'password#123',
-#         'HOST': 'db',
-#         'PORT': 3306,
-#      }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my-app-db',
+        'USER': 'toor',
+        'PASSWORD': 'password123',
+        'HOST': 'db',
+        'PORT': 3306,
+     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 AUTH_USER_MODEL = 'frontend.userDetails'
 # Password validation
