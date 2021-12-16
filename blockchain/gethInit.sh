b91f5cfc423fc4d6fed9c22352d86def25cd1211
Psycho-not0078 <snbhat0708@gmail.com>
2021-12-16 18:22:40 +0530

    edited gitignore and docker compose

--- blockchain/gethInit.sh
+++ blockchain/gethInit.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-if [ ! -d /.ethereum/keystore ]; then
-    echo "/.ethereum/keystore not found, running 'geth init'..."
+if [ ! -d /root/.ethereum/keystore ]; then
+    echo "/root/.ethereum/keystore not found, running 'geth init'..."
     geth init /opt/genesis.json
     echo "...done!"
 fi
-sleep 10 
-#geth --datadir=/.ethereum/devchain --networkid 4444 --http --http.addr 0.0.0.0 --http.corsdomain "*" --http.api "admin,debug,web3,eth,txpool,personal,ethash,miner,net" --allow-insecure-unlock
+
+geth "$@"
