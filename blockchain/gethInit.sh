042f310c28a73d6a246182d97e3145d86604553e
Psycho-not0078 <snbhat0708@gmail.com>
2021-12-16 21:30:59 +0530

    geth changes

--- blockchain/gethInit.sh
+++ blockchain/gethInit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ ! -d /root/.ethereum/keystore ]; then
-    echo "/root/.ethereum/keystore not found, running 'geth init'..."
-    geth init /opt/genesis.json
-    echo "...done!"
-fi
+# if [ ! -d /root/.ethereum/keystore ]; then
+#     echo "/root/.ethereum/keystore not found, running 'geth init'..."
+geth init /opt/genesis.json
+#     echo "...done!"
+# fi
 
-geth "$@"
+# geth "$@"
