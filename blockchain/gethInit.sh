d1375055f4e90ebc73f5a6516f8780e756bc933a
sarthak248 <65854909+sarthak-banerjee@users.noreply.github.com>
2021-12-16 11:50:00 +0530

    working patch

--- blockchain/gethInit.sh
+++ blockchain/gethInit.sh
@@ -2,8 +2,8 @@
 
 if [ ! -d /.ethereum/keystore ]; then
     echo "/.ethereum/keystore not found, running 'geth init'..."
-    geth init /files/genesis.json
+    geth init /opt/genesis.json
     echo "...done!"
 fi
 sleep 10 
-geth --datadir=/.ethereum/devchain --networkid 4444 --http --http.addr 0.0.0.0 --http.corsdomain "*" --http.api "admin,debug,web3,eth,txpool,personal,ethash,miner,net" --allow-insecure-unlock
+#geth --datadir=/.ethereum/devchain --networkid 4444 --http --http.addr 0.0.0.0 --http.corsdomain "*" --http.api "admin,debug,web3,eth,txpool,personal,ethash,miner,net" --allow-insecure-unlock
