5b1892ea65372a89cbaf874275b9bcc1f19c036e
Psycho-not0078 <snbhat0708@gmail.com>
2021-12-21 22:48:40 +0530

    sol changes

--- Swift_Vote/frontend/solWarper.py
+++ Swift_Vote/frontend/solWarper.py
@@ -3,7 +3,6 @@ import web3
 from solcx import compile_source
 import random
 
-# print(w3.personal.listAccounts, len(w3.personal.listAccounts))
 w3 = web3.Web3(web3.HTTPProvider("http://127.0.0.1:8545"))
 
 def compile_source_file(file_path):
@@ -11,7 +10,6 @@ def compile_source_file(file_path):
       source = f.read()
 
    return compile_source(source)
-
 
 def deploy_contract(file_name):
     compiled_sol = compile_source_file(file_name)
@@ -33,24 +31,55 @@ def addUser(contract_handle, location):
 
 def vote(contract_handle, addr, cid, location, date):
     hsh=contract_handle.functions.vote(cid, location, date).transact({'from':addr})
-#adding start
 
+def voteCount(contract_handle, cid):
+    ret=contract_handle.functions.countVote(cid).call()
+    return ret[1]
+
+#adding start
 def addCandidate(contract_handle, name, location):
     hsh=contract_handle.functions.addCandidate(name,location).transact({"from":w3.personal.listAccounts[0]})
+    print(hsh)
 
 def listVoters(contract_handle):
     addresses=contract_handle.functions.listVoters().call()
-    print(type(addresses))
-    return addresses[1]
-    
-
+    # print(type(addresses))
+    return addresses
 #adding closex
 
-contract_source_path = 'elections.sol'
+def listCandidates(contract_handle, location):
+    ret=contract_handle.functions.listCandidates(location).call()
+    return ret
 
-contract_handle= deploy_contract(contract_source_path)
-addUser(contract_handle,"fucklife")
-listVoters(contract_handle)
+def listOfficials(contract_handle):
+    ret=contract_handle.functions.listOfficials().call()
+    return ret
+
+def changeAblity(contract_handle,address,ablityType,Val):
+    contract_handle.functions.changeAblity(address,ablityType,Val).transact({'from': w3.personal.listAccounts[0]})
+
+
+# contract_source_path = 'elections.sol'
+
+# contract_handle= deploy_contract(contract_source_path)
+# addUser(contract_handle,"fucklife")
+# addUser(contract_handle,"awdeferg")
+# addUser(contract_handle,"fucklasewrgife")
+
+# addCandidate(contract_handle,"qwerty","1")
+# addCandidate(contract_handle,"qwerty2","1")
+
+# a=listCandidates(contract_handle,"1")
+# print(a)
+
+# v1="0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+# changeAblity(contract_handle,v1,2,1)
+# changeAblity(contract_handle,v1,3,1)
+
+# a=listVoters(contract_handle)
+# print(a)
+# a=listOfficials(contract_handle)
+# print(a)
 # txinfo=w3.eth.getTransaction(contract_tester1)
 # print(txinfo)
 # print(f'Deployed {contract_id} to: {address}\n')
